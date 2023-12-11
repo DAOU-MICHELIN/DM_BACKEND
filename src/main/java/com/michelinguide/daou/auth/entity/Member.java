6b4a4ac5c7b6c664d7de7d9b4c8aa7ce889cc617
yess98 <yesiam@daou.co.kr>
2023-12-11 16:33:09 +0900

    feat : 회원 엔티티 컬럼 수정

--- src/main/java/com/michelinguide/daou/auth/entity/Member.java
+++ src/main/java/com/michelinguide/daou/auth/entity/Member.java
@@ -12,11 +12,15 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Member {
+@SQLDelete(sql = "UPDATE member SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted = false" )
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,10 +36,13 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    private boolean isDeleted = false;
+
     @Builder
     public Member(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
+        this.role = Role.MEMBER;
     }
 }
