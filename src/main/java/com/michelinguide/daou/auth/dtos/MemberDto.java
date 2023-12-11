2073e6145eb3852052ae0c59a21429a24c31dfa9
yess98 <yesiam@daou.co.kr>
2023-12-11 10:36:50 +0900

    feat : 회원가입 요청 Dto

---
+++ src/main/java/com/michelinguide/daou/auth/dtos/MemberDto.java
@@ -0,0 +1,26 @@
+package com.michelinguide.daou.auth.dtos;
+
+import com.michelinguide.daou.auth.entity.Member;
+import lombok.Getter;
+
+public class MemberDto {
+
+    @Getter
+    public static class JoinRequest {
+
+        private String email;
+
+        private String password;
+
+        private String nickname;
+
+        public Member toEntity () {
+            return Member.builder()
+                         .email(email)
+                         .password(password)
+                         .nickname(nickname)
+                         .build();
+        }
+    }
+
+}
