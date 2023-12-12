15790936b7452c92282a4f7ac21b3b7a0ab89bd8
yess98 <yesiam@daou.co.kr>
2023-12-12 09:45:55 +0900

    feat : member 엔티티에 인증 여부 컬럼 추가

--- src/main/java/com/michelinguide/daou/auth/entity/Member.java
+++ src/main/java/com/michelinguide/daou/auth/entity/Member.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -38,11 +37,17 @@ public class Member extends BaseEntity{
 
     private boolean isDeleted = false;
 
+    private boolean isVerified = false;
+
     @Builder
     public Member(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.role = Role.MEMBER;
+    }
+
+    public void verifyEmail() {
+        this.isVerified = true;
     }
 }
