7bb9ca30b6aa1f38948f5e5f24e3e3938c6f2ce2
yess98 <yesiam@daou.co.kr>
2023-12-12 09:47:07 +0900

    feat : 이메일로 조회하는 쿼리 추가

--- src/main/java/com/michelinguide/daou/auth/repository/MemberRepository.java
+++ src/main/java/com/michelinguide/daou/auth/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.michelinguide.daou.auth.repository;
 
 import com.michelinguide.daou.auth.entity.Member;
 import java.util.Optional;
+import java.util.OptionalInt;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
 
 }
