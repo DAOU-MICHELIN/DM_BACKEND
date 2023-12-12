c8fd1c919cc623b88f2c6dedcbc11297dd30d559
yess98 <yesiam@daou.co.kr>
2023-12-12 10:37:28 +0900

    feat : ApiResponse 적용

--- src/main/java/com/michelinguide/daou/auth/controller/MemberController.java
+++ src/main/java/com/michelinguide/daou/auth/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.michelinguide.daou.auth.controller;
 import com.michelinguide.daou.auth.dtos.EmailDto;
 import com.michelinguide.daou.auth.dtos.MemberDto;
 import com.michelinguide.daou.auth.service.MemberService;
+import com.michelinguide.daou.common.code.ApiResponseCode;
+import com.michelinguide.daou.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Response;
@@ -19,14 +21,14 @@ public class MemberController {
 
     private final MemberService memberService;
     @PostMapping("/join")
-    ResponseEntity join(@RequestBody MemberDto.JoinRequest request){
+    ApiResponse join(@RequestBody MemberDto.JoinRequest request){
         Long savedId = memberService.save(request);
         log.info("Registered ID = {}", savedId);
-        return new ResponseEntity(HttpStatus.CREATED);
+        return ApiResponse.changeSuccess(ApiResponseCode.CREATE_SUCCESS, savedId);
     }
     @PostMapping("/confirm-email")
-    ResponseEntity confirmEmail(@RequestBody EmailDto.ConfirmRequest emailConfirmRequestDto) {
+    ApiResponse confirmEmail(@RequestBody EmailDto.ConfirmRequest emailConfirmRequestDto) {
         memberService.confirmEmail(emailConfirmRequestDto);
-        return new ResponseEntity(HttpStatus.ACCEPTED);
+        return ApiResponse.readSuccess();
     }
 }
