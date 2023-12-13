5139a74884938c9bbaff3e7ce722f343c07906fa
yess98 <yesiam@daou.co.kr>
2023-12-13 14:53:51 +0900

    fix : response entity 적용

--- src/main/java/com/michelinguide/daou/auth/controller/MemberController.java
+++ src/main/java/com/michelinguide/daou/auth/controller/MemberController.java
@@ -10,25 +10,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
     @PostMapping("/join")
-    ApiResponse join(@RequestBody MemberDto.JoinRequest request){
+    ResponseEntity<ApiResponse> join(@RequestBody MemberDto.JoinRequest request){
         Long savedId = memberService.save(request);
-        log.info("Registered ID = {}", savedId);
-        return ApiResponse.changeSuccess(ApiResponseCode.CREATE_SUCCESS, savedId);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.changeSuccess(ApiResponseCode.CREATE_SUCCESS, savedId));
     }
     @PostMapping("/confirm-email")
-    ApiResponse confirmEmail(@RequestBody EmailDto.ConfirmRequest emailConfirmRequestDto) {
+    ResponseEntity<ApiResponse> confirmEmail(@Validated @RequestBody EmailDto.ConfirmRequest emailConfirmRequestDto) {
         memberService.confirmEmail(emailConfirmRequestDto);
-        return ApiResponse.readSuccess();
+        return ResponseEntity.ok(ApiResponse.readSuccess());
     }
 }
