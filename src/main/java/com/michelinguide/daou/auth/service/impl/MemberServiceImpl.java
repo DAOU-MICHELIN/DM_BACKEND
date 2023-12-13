652980283a1781f9ddcf53bf62dfac38b2fe863e
yess98 <yesiam@daou.co.kr>
2023-12-13 14:56:18 +0900

    feat: 인증코드 redis 적용

--- src/main/java/com/michelinguide/daou/auth/service/impl/MemberServiceImpl.java
+++ src/main/java/com/michelinguide/daou/auth/service/impl/MemberServiceImpl.java
@@ -2,58 +2,77 @@ package com.michelinguide.daou.auth.service.impl;
 
 import com.michelinguide.daou.auth.dtos.EmailDto;
 import com.michelinguide.daou.auth.dtos.MemberDto;
-import com.michelinguide.daou.auth.entity.EmailAuth;
 import com.michelinguide.daou.auth.entity.Member;
-import com.michelinguide.daou.auth.repository.EmailAuthRepository;
 import com.michelinguide.daou.auth.repository.MemberRepository;
 import com.michelinguide.daou.auth.service.MemberService;
-import java.util.UUID;
+import com.michelinguide.daou.auth.util.RedisUtil;
+import com.michelinguide.daou.common.handler.exception.CustomException;
+import java.util.Optional;
+import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    private final EmailAuthRepository emailAuthRepository;
+    private final RedisUtil redisUtil;
 
     public Long save(MemberDto.JoinRequest request) {
-        // TODO :  이메일 중복 체크 및 도메인 체크
 
 
-        EmailAuth emailAuth = EmailAuth.builder()
-                                   .email(request.getEmail())
-                                   .authCode(UUID.randomUUID().toString())
-                                   .build();
+        // TODO : 레디스 인증 코드 조회
 
-        emailAuthRepository.save(emailAuth);
         Member savedMember = memberRepository.save(request.toEntity());
 
-        // TODO : 이메일 발송 로직 추가
 
         return savedMember.getId();
+    }
+    private void checkDuplicatedEmail(String email){
+        Optional<Member> memberByEmail = memberRepository.findByEmail(email);
+        if (memberByEmail.isPresent()){
+            throw CustomException.duplicateEmailException();
+        }
+    }
+
+    private String createCode() {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+
+        for(int i = 0 ; i < 6; i ++){
+            int index = random.nextInt(3);
+            switch (index) {
+                case 0:
+                    sb.append((char) ((int) (random.nextInt(26)) + 97));
+                    break;
+                case 1:
+                    sb.append((char) ((int) (random.nextInt(26)) + 65));
+                    break;
+                case 2:
+                    sb.append((random.nextInt(10)));
+                    break;
+            }
+        }
+        return sb.toString();
     }
 
     public void delete(Long id) {
         memberRepository.deleteById(id);
     }
 
-    @Transactional
     public void confirmEmail(EmailDto.ConfirmRequest request) {
 
-        // TODO : 커스텀 exception 개발
-        EmailAuth validEmailAuthByEmail = emailAuthRepository.findValidAuthCodeByEmail(request.getEmail())
-                                                             .orElseThrow(() -> new RuntimeException("Valid email auth not found for email: " + request.getEmail()));
+        // TODO : 이메일 중복 검사
+        checkDuplicatedEmail(request.getEmail());
 
-        // TODO : 커스텀 exception 개발
-        Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow();
+        String authCode = createCode();
+        // TODO : 이메일 전송 하는 로직 연결
 
-        member.verifyEmail();
-
-        validEmailAuthByEmail.useToken();
+        redisUtil.setDataExpire(request.getEmail(), authCode, 60 *5L );
 
     }
 
